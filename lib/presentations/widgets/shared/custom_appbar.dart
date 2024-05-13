f66d6b038ff5f44f5d21dbe0d47e2cdaa76416a9
santacruzandres1 <118927363+santacruzandres1@users.noreply.github.com>
2024-05-13 15:04:34 -0300

    punto de restauracion

--- lib/presentations/widgets/shared/custom_appbar.dart
+++ lib/presentations/widgets/shared/custom_appbar.dart
@@ -1,8 +1,9 @@
-
 import 'package:cinemapedia/presentations/providers/providers.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 
+import '../../../config/domain/entities/movie.dart';
 import '../../delgates/search_delegate_movie.dart';
 
 class CustomAppBar extends ConsumerWidget {
@@ -13,28 +14,51 @@ class CustomAppBar extends ConsumerWidget {
     final colors = Theme.of(context).colorScheme;
     final titleStyle = Theme.of(context).textTheme.titleMedium;
     return SafeArea(
-      child:Padding(
+      child: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 10),
         child: SizedBox(
           width: double.infinity,
           child: Row(
             children: [
-              Icon(Icons.movie_outlined, color: colors.primary,),
-              const SizedBox(width: 5,),
-               Text('Cinemapedia', style: titleStyle),
-               const Spacer(), 
-              IconButton(onPressed: (){
-                  final movieRepository = ref.read(movieRepositoryProvider  );
-                showSearch(context: context, delegate: SearchMovieDelagate(
-                  searchMovies: movieRepository.searchMovies // aca mandamos unicamente la referencia a la funcion, para ejecutar la funcion deberiamos recibir el query asi (query)=>movieRepository.searchMovies(query)
-                ));
-              }, icon: const Icon(Icons.search))
+              Icon(
+                Icons.movie_outlined,
+                color: colors.primary,
+              ),
+              const SizedBox(
+                width: 5,
+              ),
+              Text('Cinemapedia', style: titleStyle),
+              const Spacer(),
+              IconButton(
+                  onPressed: () {
+                    //   final movieRepository = ref.read(movieRepositoryProvider  );
+                    // showSearch(context: context, delegate: SearchMovieDelagate(
+                    //   searchMovies: movieRepository.searchMovies // aca mandamos unicamente la referencia a la funcion, para ejecutar la funcion deberiamos recibir el query asi (query)=>movieRepository.searchMovies(query)
+                    // ));
+
+                    final searchedMovies = ref.read(searchedMoviesProvider);
+                    final searchQuery = ref.read(searchQueryProvider);
+
+                    showSearch<Movie?>(
+
+                            query: searchQuery,
+                            context: context,
+                            delegate: SearchMovieDelagate(
+                                initialMovies: searchedMovies,
+                                searchMovies: ref
+                                    .read(searchedMoviesProvider.notifier)
+                                    .searchMoviesByQuery))
+                        .then((movie) {
+                      if (movie == null) return;
+
+                      context.push('/movie/${movie.id}');
+                    });
+                  },
+                  icon: const Icon(Icons.search))
             ],
           ),
         ),
-      
-        
-        ),
-      );
+      ),
+    );
   }
 }
