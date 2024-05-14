c291cae72ef95a63a6e979b30fb7ecc42fa3486d
santacruzandres1 <118927363+santacruzandres1@users.noreply.github.com>
2024-05-14 10:11:49 -0300

    Seccion 16 Go Router Tabs Finished.

--- lib/presentations/screens/movies/home_screen.dart
+++ lib/presentations/screens/movies/home_screen.dart
@@ -1,109 +1,30 @@
-import 'package:cinemapedia/presentations/providers/providers.dart';
-import 'package:cinemapedia/presentations/screens/screens.dart';
+import 'package:cinemapedia/presentations/screens/views/views.dart';
 import 'package:cinemapedia/presentations/widgets/widgets.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class HomeScreen extends StatelessWidget {
   static const name = 'home-screen';
+  final int pageIndex;
 
-  const HomeScreen({super.key});
+  const HomeScreen({
+    super.key, 
+    required this.pageIndex,
+    });
 
+    final viewRoutes = const <Widget>[
+      HomeView(),
+      SizedBox(),
+      FavoritesView()
+    ];
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: _HomeView(),
-      bottomNavigationBar: const CustomBottomNavigation(),
+    return  Scaffold(
+      body: IndexedStack(
+        index: pageIndex,
+        children: viewRoutes,
+      ),
+      bottomNavigationBar: CustomBottomNavigation(currentIndex: pageIndex,),
     );
   }
 }
 
-class _HomeView extends ConsumerStatefulWidget {
-  @override
-  _HomeViewState createState() => _HomeViewState();
-}
-
-class _HomeViewState extends ConsumerState<_HomeView> {
-  @override
-  void initState() {
-    super.initState();
-    ref.read(nowPlayinMoviesProvider.notifier).loadNextPage();
-    ref.read(popularMoviesProvider.notifier).loadNextPage();
-    ref.read(upcomingMoviesProvider.notifier).loadNextPage();
-    ref.read(topRatedMoviesProvider.notifier).loadNextPage();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    final nowPlayinMovies = ref.watch(nowPlayinMoviesProvider);
-    final moviesSlideShow = ref.watch(moviesSlideShowProviders);
-    final popularMovies = ref.watch(popularMoviesProvider);
-    final upcomingMovies = ref.watch(upcomingMoviesProvider);
-    final topRatedMovies = ref.watch(topRatedMoviesProvider);
-    // return  const FullScreenLoader();
-    return CustomScrollView(slivers: [
-      const SliverAppBar(
-        title: CustomAppBar(),
-        floating: true,
-      ),
-      SliverList(
-          delegate: SliverChildBuilderDelegate(
-        (context, index) {
-          return Column(
-            children: [
-              // const CustomAppBar(),
-
-              MoviesSlideShow(movies: moviesSlideShow),
-            
-              MoviesHorizontalListView(
-                  movies: nowPlayinMovies,
-                  title: 'En Cines',
-                  subtitleDate: 'Lun 6/05',
-                  loadNextPage: () => ref
-                      .read(nowPlayinMoviesProvider.notifier)
-                      .loadNextPage()),
-                        MoviesHorizontalListView(
-                  movies: popularMovies,
-                  title: 'Populares',
-                  // subtitleDate: 'Lun 6/05',
-                  loadNextPage: () => ref
-                      .read(popularMoviesProvider.notifier)
-                      .loadNextPage()),
-              
-              MoviesHorizontalListView(
-                  movies: topRatedMovies,
-                  title: 'Mejores Calificadas',
-                  subtitleDate: 'De todos los tiempos',
-                  loadNextPage: () => ref
-                      .read(topRatedMoviesProvider.notifier)
-                      .loadNextPage()),
-                      MoviesHorizontalListView(
-                  movies: upcomingMovies,
-                  title: 'Proximamente',
-                  subtitleDate: 'Este mes',
-                  loadNextPage: () => ref
-                      .read(upcomingMoviesProvider.notifier)
-                      .loadNextPage()),
-              const SizedBox(
-                height: 120,
-              )
-              // Expanded(
-              //   child: ListView.builder(
-              //         itemCount: nowPlayinMovies.length,
-              //         itemBuilder:(context, index) {
-              //   final movie = nowPlayinMovies[index];
-              //           return ListTile(
-              //       title: Text(movie.title),);
-              //         },
-
-              //         ),
-              // )
-            ],
-          );
-        },
-        childCount: 1,
-      )),
-    ]);
-  }
-}
-  
--- lib/presentations/widgets/shared/custom_bottom_navigation.dart
+++ lib/presentations/widgets/shared/custom_bottom_navigation.dart
@@ -1,15 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 class CustomBottomNavigation extends StatelessWidget {
-  const CustomBottomNavigation({super.key});
+  const CustomBottomNavigation({super.key, required this.currentIndex});
+  final int currentIndex;
+  void onItemTapped(BuildContext context, int index){
+    switch(index){
+    case 0:
+    context.go('/home/0');
+    break;
+    case 1:
+    context.go('/home/1');
+    break;
 
+    case 2:
+    context.go('/home/2');
+    break;
+    }
+  }
   @override
   Widget build(BuildContext context) {
     return BottomNavigationBar(
+      currentIndex: currentIndex,
+      onTap: (value) => onItemTapped(context, value),
       elevation: 0,
       items: const [
-        BottomNavigationBarItem(icon: Icon(Icons.home_outlined), label: 'Home', activeIcon: Icon(Icons.home) ),
-        BottomNavigationBarItem(icon: Icon(Icons.category_outlined), label: 'Categories', activeIcon: Icon(Icons.category)),
-        BottomNavigationBarItem(icon: Icon(Icons.favorite_border_outlined), label: 'Favorites', activeIcon: Icon(Icons.favorite)),
+        BottomNavigationBarItem(icon: Icon(Icons.home_outlined), label: 'Home', activeIcon: Icon(Icons.home_outlined) ),
+        BottomNavigationBarItem(icon: Icon(Icons.category_outlined), label: 'Categories', activeIcon: Icon(Icons.category_outlined)),
+        BottomNavigationBarItem(icon: Icon(Icons.favorite_border_outlined), label: 'Favorites', activeIcon: Icon(Icons.favorite_outline)),
 
 
       ]
