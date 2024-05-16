5b58fc35ecf981a136b2c845c5cf4e2a2942d6a9
VillalbaLeandro <118927363+santacruzandres1@users.noreply.github.com>
2024-05-16 17:59:36 -0300

    .

--- lib/presentations/screens/movies/movie_screen.dart
+++ lib/presentations/screens/movies/movie_screen.dart
@@ -199,7 +199,8 @@ class _ActorsByMovie extends ConsumerWidget {
                           child: Text(
                             actor.character ?? '',
                             style: const TextStyle(
-                                color: Colors.white, fontWeight: FontWeight.w400),
+                                color: Colors.white,
+                                fontWeight: FontWeight.w400),
                             textAlign: TextAlign.start,
                             maxLines: 2,
                             overflow: TextOverflow.ellipsis,
@@ -214,7 +215,7 @@ class _ActorsByMovie extends ConsumerWidget {
                     textAlign: TextAlign.start,
                     maxLines: 2,
                   ),
-            
+
                   //Actor Name
                 ],
               ),
@@ -242,46 +243,62 @@ class _CustomSliverAppBar extends StatelessWidget {
           titlePadding: const EdgeInsets.symmetric(horizontal: 15, vertical: 5),
           title: Text(
             movie.title,
-            style: const TextStyle(fontSize: 20),
+            style: const TextStyle(fontSize: 20, color: Colors.white),
             textAlign: TextAlign.start,
           ),
           background: Stack(
             children: [
-              SizedBox(
+              SizedBox.expand(
                 child: Image.network(
                   movie.posterPath,
                   fit: BoxFit.cover,
                   loadingBuilder: (context, child, loadingProgress) {
-                    if(loadingProgress!=null)return const SizedBox();
+                    if (loadingProgress != null) return const SizedBox();
                     return FadeIn(
-                      duration: const Duration(milliseconds: 600),
-                      child: child);
-                  } ,
+                        duration: const Duration(milliseconds: 600),
+                        child: child);
+                  },
                 ),
               ),
-              const SizedBox.expand(
-                child: DecoratedBox(
-                    decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                    begin: Alignment.topCenter,
-                    end: Alignment.bottomCenter,
-                    stops: [0.7, 1],
-                    colors: [Colors.transparent, Colors.black87],
-                  ),
-                )),
+            
+              const _CustomGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [Colors.transparent, Colors.black54, Colors.black54],
+                stops: [0.5, 0.65, 1],
               ),
-              const SizedBox.expand(
-                child: DecoratedBox(
-                    decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                    begin: Alignment.topLeft,
-                    stops: [0.0, 0.2],
-                    colors: [Colors.black87, Colors.transparent],
-                  ),
-                )),
-              ),
+              // const _CustomGradient(begin: begin, end: end, colors: colors, stops: stops)
             ],
           )),
+    );
+  }
+}
+
+class _CustomGradient extends StatelessWidget {
+  final AlignmentGeometry begin;
+  final AlignmentGeometry end;
+  final List<double>? stops;
+  final List<Color> colors;
+
+  const _CustomGradient({
+    required this.begin,
+    required this.end,
+    required this.colors,
+    required this.stops,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox.expand(
+      child: DecoratedBox(
+          decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: begin,
+          end: end,
+          stops: stops,
+          colors: colors,
+        ),
+      )),
     );
   }
 }
